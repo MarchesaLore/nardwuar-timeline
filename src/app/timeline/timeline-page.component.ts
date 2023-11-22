f9d027985b3b1d250b63358d21d5ec81b9643cc2
Lorena Marchesin <marchesin.lorena.90@gmail.com>
2023-11-22 13:52:51 +0100

    changed to vertical timeline

--- src/app/timeline/timeline-page.component.ts
+++ src/app/timeline/timeline-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, HostListener } from '@angular/core';
 import { InterviewService } from '../interviews/interview.service';
 import { IInterview } from '../interviews/interview';
 
@@ -10,17 +10,24 @@ import { IInterview } from '../interviews/interview';
 export class TimelinePageComponent{
   interviews: any[] = [];
   selectedInterview: any;
+  isSticky = false;
 
   onInterviewSelected(interview: any): void {
     this.selectedInterview = interview;
   }
   constructor(private interviewService: InterviewService) {}
 
+ 
   ngOnInit() {
     this.interviewService.getInterviews().subscribe(data => {
       this.interviews = data.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
       //console.log(data);
     });
   }
-
+  
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    // Set the sticky condition based on the scroll position
+    this.isSticky = window.scrollY > 500; // Adjust the value based on when you want it to become sticky
+  }
 }
