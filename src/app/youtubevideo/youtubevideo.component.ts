8f228131c07dff32aab3ab7c0c7c6170fe951d10
Lorena Marchesin <marchesin.lorena.90@gmail.com>
2024-01-03 16:32:15 +0100

    validating id before loading the video

--- src/app/youtubevideo/youtubevideo.component.ts
+++ src/app/youtubevideo/youtubevideo.component.ts
@@ -11,9 +11,19 @@ export class YoutubeEmbedComponent {
 
   constructor(private sanitizer: DomSanitizer) {}
 
-  //sanatize to prevent security issues
+  // Sanitize to prevent security issues
   getVideoUrl(): SafeResourceUrl {
+    if (!this.isValidYoutubeVideoId(this.youtubeVideoId)) {
+      throw new Error('Invalid YouTube Video ID');
+    }
+
     const youtubeUrl = `https://www.youtube.com/embed/${this.youtubeVideoId}`;
     return this.sanitizer.bypassSecurityTrustResourceUrl(youtubeUrl);
+  }
+
+  // Validate YouTube Video ID format
+  private isValidYoutubeVideoId(videoId: string): boolean {
+    const youtubeVideoIdRegex = /^[a-zA-Z0-9_-]{11}$/;
+    return youtubeVideoIdRegex.test(videoId);
   }
 }
