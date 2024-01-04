3963a4886572522674d24e685af660c8e8daae24
Lorena Marchesin <marchesin.lorena.90@gmail.com>
2024-01-04 21:29:45 +0100

    adding images to homepage

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,34 +1,30 @@
-import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss'],
-  animations: [
-    trigger('fadeInOut', [
-      state('in', style({ opacity: 1 })),
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate(500)
-      ]),
-      transition(':leave', [
-        animate(500, style({ opacity: 0 }))
-      ])
-    ])
-  ]
+  styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent {
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
-
-  @HostListener('window:scroll', ['$event'])
-  checkScroll() {
-    const componentPosition = this.el.nativeElement.offsetTop;
-    const scrollPosition = window.pageYOffset;
-
-    if (scrollPosition >= componentPosition) {
-      this.renderer.addClass(this.el.nativeElement, 'fadeInOut');
-    } else {
-      this.renderer.removeClass(this.el.nativeElement, 'fadeInOut');
-    }
-  }
+  sections = [
+    {
+      title: 'Who is Nardwuar?',
+      content: 'Nardwuar is a bizarre-looking Canadian journalist that has interviewed many musicians. His interviews always have some deep cuts, and he often gives gifts that surprise the interviewer.',
+      image: '/assets/nardwuar.png',
+      alt: 'image of nardwuar 1',
+    },
+    {
+      title: 'Interview style',
+      content: 'Noted for his excitable persona, a typical Nardwuar interview will begin with "Who are you?", followed by "From?" if the subject does not volunteer their affiliations.',
+      image: '/assets/nardwuar1.png',
+      alt: 'image of nardwuar 2',
+    },
+    {
+      title: 'Origin of the name',
+      content: 'When asked to explain his name, Nardwuar has said it is \"a dumb, stupid name like Sting or Sinbad\"; that \"Human\" came from the song "Human Fly" by the Cramps; and that \"Serviette\" came from the fact that "in the U.S.A. they don\'t have serviettes, they have napkins\".  ',
+      image: '/assets/nardwuar2.png',
+      alt: 'image of nardwuar 2',
+    },
+  ];
 }
